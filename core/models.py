2f4d74b6611fc0c78f7db92d8329b0a823c84c33
LuigiPerkoski <celluigipplc@gmail.com>
2024-05-02 20:04:07 -0300

    make models (Lider, Funcionario)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Funcionario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    horario_entrada = models.IntegerField()
+    intervalo = models.IntegerField()
+    horario_almoco = models.IntegerField()
+    horario_saida = models.IntegerField()
+    duracao_intervalo = models.IntegerField()
+    atividade = models.BooleanField()
+    cor = models.CharField(max_length=20, choices=[("cinza", "Cinza"), ("amarelo", "Amarelo"), ("verde", "Verde"), ("vermelho", "Vermelho")])
+
+    def __str__(self):
+        return f"Funcionario {self.usuario.username}"
+
+class Lider(models.Model):
+    funcionarios = models.ManyToManyField(Funcionario)
+
+    def __str__(self):
+        return "Lider"
